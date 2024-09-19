9e4343cab30f5f16ba09a21093271a97609b36b9
Raul <149816750+raulventura04@users.noreply.github.com>
2024-09-19 14:52:40 -0300

    Add files via upload

---
+++ Respostas/Questão_2.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    a, b = 0, 1
+    while a <= n:
+        if a == n:
+            return f"O número {n} pertence à sequência de Fibonacci."
+        a, b = b, a + b
+    return f"O número {n} não pertence à sequência de Fibonacci."
+
+# Exemplo de uso
+numero = int(input("Informe um número: "))
+print(fibonacci(numero))
